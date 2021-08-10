75e1c883b57a78dee27e3967919498b3c0cb7d2c
sadyko <sadyvitor@hotmail.com>
2021-08-10 20:05:27 -0300

    Desafio das camisas att

---
+++ tamanho das camisas.js
@@ -0,0 +1,9 @@
+function solucao(tamanhoBr) {
+  
+    if (tamanhoBr == "PP") console.log ("XS");
+    if (tamanhoBr == "P") console.log ("S");
+    if (tamanhoBr == "M") console.log ("M");
+    if (tamanhoBr == "G") console.log ("L");
+    if (tamanhoBr == "GG") console.log("XL");
+  
+}
