2058c28cf1d871bef33ff905f2ad11fb73f83f03
sadyko <sadyvitor@hotmail.com>
2021-08-10 21:18:56 -0300

    Desafio do Esconde

---
+++ Esconde-esconde.js
@@ -0,0 +1,11 @@
+
+function solucao(numero) {
+	if (numero <= 20) console.log("VOCE CONSEGUE, MANINHO");
+    else {
+        for(inicio = 21;inicio<=numero;inicio++) {
+            console.log(inicio);
+        }
+    
+    
+    }
+}
